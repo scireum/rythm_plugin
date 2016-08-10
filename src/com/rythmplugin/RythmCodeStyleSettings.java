9e5501aabba632e4f4c734237677909f2dd54c8a
mpl <mpl@scireum.de>
2016-08-10 16:58:40 +0200

    Möglichkeit Rythm Code Style zu ändern wurde hinzugefügt
    Problem: Rythm wird nach language injection nur zwischen Tags erkannt.

---
+++ src/com/rythmplugin/RythmCodeStyleSettings.java
@@ -0,0 +1,13 @@
+package com.rythmplugin;
+
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+
+/**
+ * Created by mpl on 10.08.2016.
+ */
+public class RythmCodeStyleSettings extends CustomCodeStyleSettings{
+    public RythmCodeStyleSettings(CodeStyleSettings settings){
+        super("RythmCodeStyleSettings", settings);
+    }
+}
---
+++ src/com/rythmplugin/RythmCodeStyleSettingsProvider.java
@@ -0,0 +1,49 @@
+package com.rythmplugin;
+
+import com.intellij.application.options.CodeStyleAbstractConfigurable;
+import com.intellij.application.options.CodeStyleAbstractPanel;
+import com.intellij.application.options.TabbedLanguageCodeStylePanel;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CodeStyleSettingsProvider;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
+/**
+ * Created by mpl on 10.08.2016.
+ */
+public class RythmCodeStyleSettingsProvider extends CodeStyleSettingsProvider {
+
+    @Override
+    public CustomCodeStyleSettings createCustomSettings(CodeStyleSettings settings){
+       return new RythmCodeStyleSettings(settings);
+    }
+
+    @Nullable
+    @Override
+    public String getConfigurableDisplayName(){
+        return "Rythm";
+    }
+    @NotNull
+    @Override
+    public Configurable createSettingsPage(CodeStyleSettings settings, CodeStyleSettings originalSettings){
+        return new CodeStyleAbstractConfigurable(settings, originalSettings, "Rythm") {
+            @Override
+            protected CodeStyleAbstractPanel createPanel(CodeStyleSettings settings) {
+                return new RythmCodeStyleMainPanel(getCurrentSettings(), settings);
+            }
+
+            @Nullable
+            @Override
+            public String getHelpTopic() {
+                return null;
+            }
+        };
+    }
+    private static class RythmCodeStyleMainPanel extends TabbedLanguageCodeStylePanel {
+        public RythmCodeStyleMainPanel(CodeStyleSettings currentSettings, CodeStyleSettings settings){
+            super(RythmLanguage.INSTANCE, currentSettings, settings);
+        }
+    }
+}
