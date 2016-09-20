a73f56f2d3021d0ab3cedbd16513ee7eb6e19d62
mpl <mpl@scireum.de>
2016-09-20 14:26:12 +0200

    changed the icon for rythm files

--- src/com/rythmplugin/icons/RythmIcons.java
+++ src/com/rythmplugin/icons/RythmIcons.java
@@ -7,5 +7,5 @@ import javax.swing.*;
  * Created by mpl on 04.08.2016.
  */
 public class RythmIcons {
-    public static final Icon FILE = IconLoader.getIcon("/com/rythmplugin/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("/com/rythmplugin/icons/rythm.png");
 }
